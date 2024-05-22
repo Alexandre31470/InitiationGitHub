6c9dd91c4921361d683cad79ef811f0b0e105d47
Bee042 <barbara042@hotmail.fr>
2024-05-22 09:38:40 +0200

    modif relation bdd

--- bdd/ecfblog.sql
+++ bdd/ecfblog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 21 mai 2024 à 21:28
+-- Généré le : mer. 22 mai 2024 à 07:37
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.13
 
@@ -29,13 +29,13 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
-  `identifiant` int NOT NULL AUTO_INCREMENT,
+  `id_article` int NOT NULL AUTO_INCREMENT,
   `titre` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `contenu` text COLLATE utf8mb4_general_ci NOT NULL,
   `auteur` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `date_creation` datetime DEFAULT CURRENT_TIMESTAMP,
   `categorie_id` int DEFAULT NULL,
-  PRIMARY KEY (`identifiant`),
+  PRIMARY KEY (`id_article`),
   KEY `categorie_id` (`categorie_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -47,9 +47,9 @@ CREATE TABLE IF NOT EXISTS `articles` (
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `identifiant` int NOT NULL AUTO_INCREMENT,
+  `id_categorie` int NOT NULL AUTO_INCREMENT,
   `nom_categorie` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`identifiant`)
+  PRIMARY KEY (`id_categorie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -60,12 +60,12 @@ CREATE TABLE IF NOT EXISTS `categories` (
 
 DROP TABLE IF EXISTS `commentaires`;
 CREATE TABLE IF NOT EXISTS `commentaires` (
-  `identifiant` int NOT NULL AUTO_INCREMENT,
+  `id_commentaire` int NOT NULL AUTO_INCREMENT,
   `auteur_commentaire` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `contenu` text COLLATE utf8mb4_general_ci NOT NULL,
   `date_creation` datetime DEFAULT CURRENT_TIMESTAMP,
   `article_id` int DEFAULT NULL,
-  PRIMARY KEY (`identifiant`),
+  PRIMARY KEY (`id_commentaire`),
   KEY `article_id` (`article_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -77,13 +77,13 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
 
 DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `identifiant` int NOT NULL AUTO_INCREMENT,
+  `id_utilisateur` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `prenom` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `pseudo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `mail` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `mdp` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`identifiant`)
+  PRIMARY KEY (`id_utilisateur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -94,13 +94,19 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 -- Contraintes pour la table `articles`
 --
 ALTER TABLE `articles`
-  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`identifiant`);
+  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id_categorie`);
 
 --
 -- Contraintes pour la table `commentaires`
 --
 ALTER TABLE `commentaires`
-  ADD CONSTRAINT `commentaires_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`identifiant`);
+  ADD CONSTRAINT `commentaires_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id_article`);
+
+--
+-- Contraintes pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD CONSTRAINT `utilisateurs_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `commentaires` (`id_commentaire`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
