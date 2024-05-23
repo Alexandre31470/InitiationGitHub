94f7f7710006b7cc2a9c10e6bab82d74c09469b7
Alexandre31470 <alexandre.quev@gmail.com>
2024-05-23 20:53:14 +0200

    save avant pull

--- pages/liste_articles/liste_articles.php
+++ pages/liste_articles/liste_articles.php
@@ -15,24 +15,24 @@ try {
 $title = "Titre de l'article";
 $content = "Contenu de l'article";
 
-try {
-    // Préparer la requête SQL d'insertion
-    $sql = "INSERT INTO articles (title, content) VALUES (:title, :content)";
+// try {
+//     // Préparer la requête SQL d'insertion
+//     $sql = "INSERT INTO articles (title, content) VALUES (:title, :content)";
 
-    // Initialiser la requête
-    $stmt = $pdo->prepare($sql);
+//     // Initialiser la requête
+//     $stmt = $pdo->prepare($sql);
 
-    // Lier les paramètres
-    $stmt->bindParam(':title', $title);
-    $stmt->bindParam(':content', $content);
+//     // Lier les paramètres
+//     $stmt->bindParam(':title', $title);
+//     $stmt->bindParam(':content', $content);
 
-    // Exécuter la requête
-    $stmt->execute();
+//     // Exécuter la requête
+//     $stmt->execute();
 
-    echo "Nouvel enregistrement créé avec succès";
-} catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-}
+//     echo "Nouvel enregistrement créé avec succès";
+// } catch (PDOException $e) {
+//     echo "Erreur : " . $e->getMessage();
+// }
 
 // Fermer la connexion
 $pdo = null;
